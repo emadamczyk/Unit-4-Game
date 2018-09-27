121199cc52114d0b74972da87ef9023a85f2aa26
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-09-27 00:54:43 -0500

    final tweaks in functionality plus readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Unit-4-Game
+
+This game is also posted at Also posted via my portfolio: https://emadamczyk.github.io/Bootstrap-Portfolio/portfolio.html
+
+I never attempted the Star Wars game, mainly because I couldn't relate to the premise of it. 
+
+I am glad I finally got the functionality of functions employed for the Crystal Collector Game.
+It seems to me that the logic used for the Star Wars game would be similar. 
+
+For this Crystal Collector Game, the game generates a random target number between 19-120 for each round of play. Each of four crystals is also assigned a hidden random number which is not revealed until the player clicks the crystal. Each click adds to the player's score for that round. The player's goal is to get the total score to equal the target number. If the player's score equals the target number, the player wins that round of play; if the player's score goes over the target number, the player loses that round of play.
+
+For this game, I initially created a bootstrap html page for the game. Then I added variables to the javascript and began mapping out the functions. The most challenging aspect of this game was writing the for loop to assign a new random number to each of the four crystals, without individually assigning a random number to each. Putting the crystals in an array and iterating over the array and using the .attr() method.
+
+The final aspect was getting "you win" or "you lose" to print to the DOM and to then clear that text upon the start of a new game, which I did within the on click function.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,88 @@
-//declare variables 
-var targetNumber;
-var totalScore = 0;
-var wins = 0;
-var losses = 0;
-//for each crystal image?
-//var crystalOne;
-//var crystalTwo;
-//var crystalThree;
-//var crystalFour;
+$(document).ready(function() {
+  //declare variables
+  var targetNumber;
+  var totalScore = 0;
+  var wins = 0;
+  var losses = 0;
+  var isGameOver = false;
 
-//add variables for the click events
-var crystalOne = $("#crystal-one");
-var crystalTwo = $("#crystal-two");
-var crystalThree = $("#crystal-three");
-var crystalFour = $("#crystal-four");
+  //add variables for the click eventss
+  var crystalOne = $("#crystal-one");
+  var crystalTwo = $("#crystal-two");
+  var crystalThree = $("#crystal-three");
+  var crystalFour = $("#crystal-four");
 
-// create array for all crystals
-var crystals = [crystalOne, crystalTwo, crystalThree, crystalFour];
+  // create array for all crystals
+  var crystals = [crystalOne, crystalTwo, crystalThree, crystalFour];
 
+  //get targetNumber using random(), generated for each new game
+  function getRandomTargetNumber() {
+    var randomTarget = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
-//get targetNumber using random(), generated for each new game
-function getRandomTargetNumber() { 
-	randomTarget = (Math.floor(Math.random() * (120 - 19 + 1)) + 19);
-	
     return randomTarget;
-};
-console.log(getRandomTargetNumber());
+  }
+  console.log(getRandomTargetNumber());
 
-//get crystalNumber using random(), one for each crystal
-function getCrystalNumber() {
-	randomCrystal =  (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
-	
+  //get crystalNumber using random(), one for each crystal
+  function getCrystalNumber() {
+    var randomCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+
     return randomCrystal;
-};
-console.log(getCrystalNumber());
+  }
+  console.log(getCrystalNumber());
 
-
-// goal to match total score to the random target number
-// when match happens, log win and a "you won"
-// if total score goes over the target number, log a loss and "you lost"
-// reset game to new random target number (19-120) and total score back to zero, and each crystal gets new random number (1-12) attached to it.
-function gameStatus() {
-	if (targetNumber === totalScore){
-        alert("You Won!!!"); 
-        $("<div>").text("You won!"); // will this write to the DOM?
-		wins++;
-		totalScore = 0;	
-		newGame();
-	
-	}else if(totalScore > gameNumber) {
-		alert("You lost! Get your counting skills together."); // how write this to the DOM?
-		losses++;
-		totalScore = 0;
-		newGame();
-		
-	}
-}
-
-// to play, with each click of crystal, add that many points to total score
-function newGame() {	
-$(document).ready(function (){
-
-	//print initial game settings to page;
-	$("#wins").text(wins);
-	$("#losses").text(losses); 
-    $("#total_score").text(totalScore);
-    //$(.crystal-images).empty(); // needed step to clear previous random values?
-	
-	//set new random target value
-    targetNumber = getRandomTargetNumber();
-    //set new random crystal values to each distinct crystal image
-    //var crystalOne = getCrystalNumber();
-	//var crystalTwo = 0;
-	//var crystalThree = 0;
-    //var crystalFour = 0;
-    
-    for (var i = 0; i < crystals.length; i++){
-        getCrystalNumber();
-        crystals[i].attr({ //could I use this here?
-            "data-random":random
-        });    
+  // goal to match total score to the random target number
+  // when match happens, log win and a "you won"
+  // if total score goes over the target number, log a loss and "you lost"
+  // reset game to new random target number (19-120) and total score back to zero, and each crystal gets new random number (1-12) attached to it.
+  function gameStatus() {
+    if (targetNumber === totalScore) {
+      //alert("You Won!!!");
+      isGameOver = true;
+      $("#alert-holder").text("You won! You're a counting rockstar!");
+      wins++;
+      totalScore = 0;
+      newGame();
+    } else if (totalScore > targetNumber) {
+      isGameOver = true;
+      //alert("You Lost! Get your counting skills together.");
+      $("#alert-holder").text("You lost! Get your counting skills together.");
+      losses++;
+      totalScore = 0;
+      newGame();
     }
+  }
 
+  // to play, with each click of crystal, add that many points to total score
+  function newGame() {
+    isGameOver = false;
+    //print initial game settings to page;
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#score").text(totalScore);
+     
+    //set new random target value
+    targetNumber = getRandomTargetNumber();
+    
+    //set new random value for each crystal
+    for (var i = 0; i < crystals.length; i++) {
+      crystals[i].attr("data-random", getCrystalNumber());
+    }
+    
+    // print gameNumber to html page.
+    $("#target-number").text(targetNumber);
+  }
 
-	// print gameNumber to html page.
-	 $("#target-number").text(targetNumber);
-	
-	// console logging to see crystal values until game development is complete.
-	console.log(`this is the current game's crystal values: (${crystalOne}, ${crystalTwo}, ${crystalThree}, ${crystalFour})`);
-	
-	
-	$("#total_score").text(totalScore);
-	
-	
-	//});
+  $(".crystal-images").on("click", function() {
+    $("#alert-holder").empty();  
+    //console.log(this, $(this))
+    var number = $(this).attr("data-random");
+    totalScore += parseInt(number);
+    console.log(totalScore);
+    $("#score").text(totalScore);
+    gameStatus();
+  });
+
+  //call a new game
+  newGame();
 });
-//call a new game
-newGame();
